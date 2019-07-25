a1037f76082e4e6c9adcb6f884d63368b57dd359
Ro297 <rohan.poddar_ug19@ashoka.edu.in>
2019-07-25 17:52:23 +0530

    Models added and changed

--- summarizer.py
+++ summarizer.py
@@ -17,6 +17,8 @@ article_text = ""
 for p in paragraphs:
     article_text += p.text
 
+#print(article_text.encode("utf-8"))
+
 # Removing Square Brackets and Extra Spaces
 article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
 article_text = re.sub(r'\s+', ' ', article_text)
@@ -26,6 +28,7 @@ formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
 formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
 sentence_list = sent_tokenize(article_text)
+
 
 stopwords = nltk.corpus.stopwords.words('english')
 
@@ -44,16 +47,15 @@ for word in word_frequencies.keys():
 
 sentence_scores = {}
 for sent in sentence_list:
-    for word in word_tokenize(sent.lower()):
-        if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
-                if sent not in sentence_scores.keys():
-                    sentence_scores[sent] = word_frequencies[word]
-                else:
-                    sentence_scores[sent] += word_frequencies[word]
+	for word in word_tokenize(sent.lower()):
+		if word in word_frequencies.keys():
+			if len(sent.split(' ')) < 30:
+				if sent not in sentence_scores.keys():
+					sentence_scores[sent] = word_frequencies[word]
+				else:
+					sentence_scores[sent] += word_frequencies[word]
 
-
-summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+summary_sentences = heapq.nlargest(10, sentence_scores, key=sentence_scores.get)
 
 summary = ' '.join(summary_sentences)
 print(summary)
